2ab12ed80c2c973283f0009de94c0f5f8fcbe89e
JL-Joson <1250377062@qq.com>
2021-04-17 22:32:29 +0800

    Create widget.h

---
+++ OTSU/otsu/widget.h
@@ -0,0 +1,27 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
